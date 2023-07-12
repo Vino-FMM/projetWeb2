840d1cb3cd4e7d8245de8be489ac53624f955514
faroukmanai <79013591+faroukmanai@users.noreply.github.com>
2023-07-12 15:46:24 -0400

    ajout d'un modal pour confirmer la supression d'un cellier

--- resources/views/welcomee.blade.php
+++ resources/views/welcomee.blade.php
@@ -19,14 +19,11 @@
                     <a href="#">{{ $cellier->nom_cellier }}</a>  
                     <div>
                         <a href="{{ route('cellier.edit', ['id' => $cellier->id]) }}"><img src="https://s2.svgbox.net/hero-outline.svg?ic=pencil&color=000000" width="32" height="32"></a>
-                        <div>
-                            <a href="{{ route('cellier.destroy', ['id' => $cellier->id]) }}" onclick="event.preventDefault(); document.getElementById('delete-form-{{ $cellier->id }}').submit();">
-                                <img src="https://s2.svgbox.net/materialui.svg?ic=delete&color=000" width="32" height="32">
-                            </a>
-                            <form id="delete-form-{{ $cellier->id }}" action="{{ route('cellier.destroy', ['id' => $cellier->id]) }}" method="POST" style="display: none;">
-                                @csrf
-                                @method('DELETE')
-                            </form>
+                        <a href="#" data-cellier-id="{{ $cellier->id }}"><img src="https://s2.svgbox.net/materialui.svg?ic=delete&color=000" width="32" height="32"></a>
+                        <form id="delete-form-{{ $cellier->id }}" action="{{ route('cellier.destroy', ['id' => $cellier->id]) }}" method="POST" style="display: none;">
+                            @csrf
+                            @method('DELETE')
+                        </form>
                     </div>  
                 </div>
             </div>
@@ -49,4 +46,42 @@
     </div>
 @endif
 </main>
+<!--  Confirmation de supression Modal -->
+<div id="deleteModal" class="modal">
+    <div class="modal-content">
+        <p>Êtes-vous sûr de vouloir supprimer ce cellier?</p>
+        <button id="closeBtn" class="close-button">&times;</button>
+        <button id="confirmBtn" class="confirm-button">Supprimer</button>
+    </div>
+</div>
+
+<script>
+let deleteModal = document.getElementById("deleteModal");
+let closeBtn = document.getElementById("closeBtn");
+let confirmBtn = document.getElementById("confirmBtn");
+
+document.querySelectorAll("[data-cellier-id]").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        let deleteFormId = "delete-form-" + btn.getAttribute("data-cellier-id");
+        confirmBtn.onclick = function () {
+            document.getElementById(deleteFormId).submit();
+        };
+        deleteModal.style.display = "block";
+    });
+});
+
+closeBtn.addEventListener("click", () => {
+    deleteModal.style.display = "none";
+});
+
+window.onclick = (event) => {
+    if (event.target == deleteModal) {
+        deleteModal.style.display = "none";
+    }
+};
+</script>
+
+
+
 @endsection
