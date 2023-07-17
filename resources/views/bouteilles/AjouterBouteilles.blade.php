d86945e21a87b5a9d5e89703485a0d98e1c1687c
faroukmanai <79013591+faroukmanai@users.noreply.github.com>
2023-07-17 17:44:39 -0400

    css carte bouteilles dans liste des bouteilles

--- resources/views/bouteilles/AjouterBouteilles.blade.php
+++ resources/views/bouteilles/AjouterBouteilles.blade.php
@@ -1,30 +1,49 @@
 @extends('layouts.app')
 @section('title', 'Liste des bouteilles')
 @section('content')
-    <div>
-        <h1>Liste des bouteilles</h1>
-        <div>
+    <main>
+        <div class="header">
+            <h2>Liste des bouteilles</h2>
+        </div> 
+        <div class="container-bouteilles">
             @foreach($bottles as $bottle)
-                <div>
-                    <div>
-                        <img src="{{ $bottle->url_img }}" alt="{{ $bottle->nom }}" style="max-width: 100%; height: auto;">
-                        <div>
+                <div class="carte-bouteille">
+                    <img src="{{ $bottle->url_img }}" alt="{{ $bottle->nom }}" style="max-width: 100%; height: auto;">
+                        <div class="carte-details">
                             <h5>{{ $bottle->nom }}</h5>
                             <p>{{ $bottle->type }} | {{ $bottle->format }} | {{ $bottle->pays }}</p>
-                            <p>{{ $bottle->prix }}</p>
+                            <p>{{ $bottle->prix }} $</p>
+                            <!-- <small>{{ $bottle->code_saq }}</small> -->
                             <div>
                                 <form method="POST" action="{{ route('bouteilles.addBouteille', ['id' => $bottle->id]) }}">
                                     @csrf
-                                    <button type="submit"><i class="bi bi-patch-plus"></i>+</button>
-                                </form>
-                                <small>{{ $bottle->code_saq }}</small>
+                                    <button type="submit" class="bouton ajout-bouteille"><i class="bi bi-patch-plus"></i>Ajouter au cellier</button>
+                                </form>  
                             </div>
                         </div>
-                    </div>
                 </div>
             @endforeach
-            
+
             {{ $bottles->links('vendor.pagination.custom') }}
         </div>
+    </main>
+
+<footer>
+    <div>
+        <a href="{{ route('home') }}"><img src="https://s2.svgbox.net/octicons.svg?ic=home&color=000" width="32" height="32"></a>
+        <span>Acueil</span>
     </div>
+    <div>
+    <img src="https://s2.svgbox.net/hero-outline.svg?ic=plus-sm&color=000000" width="32" height="32">
+        <span>Ajout</span>
+    </div>
+    <div>
+    <img src="https://s2.svgbox.net/octicons.svg?ic=search&color=000" width="32" height="32">
+        <span>Recherche</span>
+    </div>
+    <div>
+    <img src="https://s2.svgbox.net/materialui.svg?ic=list&color=000" width="32" height="32">
+        <span>Liste</span>
+    </div>
+</footer>
 @endsection
