2fee5cffd3036a0337f6b447251b9b9c8194469f
Montasser <montasser.zid19@gmail.com>
2023-08-09 10:24:13 -0400

    ne pas afficher le message disant que Vouz avez dej cette bouteille

--- resources/views/bouteilles/AjouterBouteilles.blade.php
+++ resources/views/bouteilles/AjouterBouteilles.blade.php
@@ -6,7 +6,8 @@
         <div class="header">
             <h4>Liste des bouteilles</h4>
             <small>cellier: {{ $mon_cellier->nom_cellier }}</small>
-        </div> 
+        </div>
+        <a href="{{ route('Ajouter-bouteille-manuellement', ['cellier_id' => $mon_cellier->id]) }}">Ajouter une bouteille manuellement</a> 
             <div class="container-recherche">
                 <input type="text" id="searchInput" placeholder="Rechercher une bouteille" >
                 <div id="searchResults"></div>
@@ -52,48 +53,39 @@
                 </form>
                 <a class="toggle-filter" href="#"><img src='https://s2.svgbox.net/materialui.svg?ic=filter_alt'></a>
             </div> -->
-        <div class="container-bouteilles">
-            @foreach($bottles as $bottle)
-                <div class="carte-bouteille">
-                    <div>
-                        <!-- <div class="cercle {{ $bottle->type === 'Vin rouge' ? 'cercle-rouge' : ($bottle->type === 'Vin blanc' ? 'cercle-doré' : '') }}"></div> -->
-                        <div class="cercle
-                            {{ $bottle->type === 'Vin rouge' ? 'cercle-rouge' : ($bottle->type === 'Vin blanc' ? 'cercle-doré' : ($bottle->type === 'Vin rosé' ? 'cercle-rose' : ($bottle->type === 'Vin de tomate' ? 'cercle-tomate' : ($bottle->type === 'Vin de dessert' ? 'cercle-bleu' : '')))) }}">
+            <div class="container-bouteilles">
+        @foreach($bottles as $bottle)
+        <div class="carte-bouteille">
+            <div>
+                <div class="cercle
+                    {{ $bottle->type === 'Vin rouge' ? 'cercle-rouge' : ($bottle->type === 'Vin blanc' ? 'cercle-doré' : ($bottle->type === 'Vin rosé' ? 'cercle-rose' : ($bottle->type === 'Vin de tomate' ? 'cercle-tomate' : ($bottle->type === 'Vin de dessert' ? 'cercle-bleu' : '')))) }}">
+                </div>
+                <img src="{{ $bottle->url_img }}" alt="{{ $bottle->nom }}" style="max-width: 100%; height: auto;">
+            </div>  
+            <div class="carte-details">
+                <h4>{{ $bottle->nom }}</h4>
+                <small>{{ $bottle->type }} | {{ $bottle->format }} | {{ $bottle->pays }}</small>
+                <small> prix: {{ $bottle->prix }} $</small>
+                <small>code SAQ: {{ $bottle->code_saq }}</small>
+                <div>
+                    <form method="POST" action="{{ route('bouteilles.addBouteille', ['id' => $bottle->id]) }}">
+                        @csrf
+                        <input type="hidden" name="cellier_id" value="{{ $cellier_id }}">
+                        <div class='container-incrementation'>
+                            <label for="quantite"><small>Qté: </small></label>
+                            <button type="button" onclick="decrementQuantity(this.parentElement)">-</button>
+                            <input id="quantity-input" type="number" name="quantite" value="1" min="1" max="99" class="quantite">
+                            <button type="button" onclick="incrementQuantity(this.parentElement)">+</button>
                         </div>
-                        <img src="{{ $bottle->url_img }}" alt="{{ $bottle->nom }}" style="max-width: 100%; height: auto;">
-                    </div>  
-                    <div class="carte-details">
-                        <h4>{{ $bottle->nom }}</h4>
-                        <small>{{ $bottle->type }} | {{ $bottle->format }} | {{ $bottle->pays }}</small>
-                        <small> prix: {{ $bottle->prix }} $</small>
-                        <small>code SAQ: {{ $bottle->code_saq }}</small>
-                        <div>
-                            @if(in_array($bottle->code_saq, $owned_bottles))
-                                <p type="button" class="bouton-disabled" disabled>
-                                <img src="https://s2.svgbox.net/octicons.svg?ic=check&color=000" width="15" height="15">vous avez déja cette bouteille!
-                                </p>
-                            @else
-                            <form method="POST" action="{{ route('bouteilles.addBouteille', ['id' => $bottle->id]) }}">
-                                @csrf
-                                <input type="hidden" name="cellier_id" value="{{ $cellier_id }}">
-                                <div class='container-incrementation'>
-    <label for="quantite"><small>Qté: </small></label>
-    <button type="button" onclick="decrementQuantity(this.parentElement)">-</button>
-    <input id="quantity-input" type="number" name="quantite" value="1" min="1" max="99" class="quantite">
-    <button type="button" onclick="incrementQuantity(this.parentElement)">+</button>
-</div>
-
-                                <button type="submit" class="bouton ajout-bouteille">Ajouter</button>
-                            </form>
-
-                            @endif
-                            </div>
-                        </div>
-                    </div>
-            @endforeach
-
-            {{ $bottles->appends(request()->query())->links('vendor.pagination.custom') }}
+                        <button type="submit" class="bouton ajout-bouteille">Ajouter</button>
+                    </form>
+                </div>
+            </div>
         </div>
+        @endforeach
+
+        {{ $bottles->appends(request()->query())->links('vendor.pagination.custom') }}
+    </div>
     </main>
 
     <footer>
