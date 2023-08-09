50d5d9bb8b46d99ba36e1fc4f7239f789803da3f
faroukmanai <79013591+faroukmanai@users.noreply.github.com>
2023-08-08 20:52:05 -0400

    Ajout de l'icone de recherche dans le footer

--- resources/views/bouteilles/addBouteilleSearch.blade.php
+++ resources/views/bouteilles/addBouteilleSearch.blade.php
@@ -44,14 +44,10 @@
     <a href="{{route('cellier.create')}}"><img src="https://s2.svgbox.net/hero-outline.svg?ic=plus-sm&color=000000" width="22" height="22"></a>
         <span>Ajout</span>
     </div>
-    <!-- <div>
-    <img src="https://s2.svgbox.net/octicons.svg?ic=search&color=000" width="32" height="32">
+    <div>
+        <a href=""><img src="https://s2.svgbox.net/octicons.svg?ic=search&color=000" width="22" height="22"></a>
         <span>Recherche</span>
     </div>
-    <div>
-    <img src="https://s2.svgbox.net/materialui.svg?ic=list&color=000" width="32" height="32">
-        <span>Liste</span>
-    </div> -->
 </footer>
 @endsection
 <script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,6 @@
         <title>Vino | cellier</title>
     
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Bootstrap icons-->
-        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" /> -->
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <!-- <link href="{{ asset('css/styles.css') }}" rel="stylesheet" /> -->
-        <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
         <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     </head>
